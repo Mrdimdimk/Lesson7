2d7da50568fbdf1dd093f3417aa1054c5caaa32f
Mrdimdimk <mrdimdimk@gmail.com>
2018-02-19 16:13:28 +0300

    Add files via upload

--- by.vashkevich.Arrys/src/by/vashkevich/StudGroup/Group.java
+++ by.vashkevich.Arrys/src/by/vashkevich/StudGroup/Group.java
@@ -1,30 +1,32 @@
 package by.vashkevich.StudGroup;
 
+import by.htp.lesson7.model.Student;
+
 public class Group {
 	private String title;
-	private Student[] studentik; // массив из студентов
-	private int studentCounter =0;
+	private Student[] studentik; // Array of students
+	private int studentCounter = 0;
 
-	public void setStudentsArray(Student[] studentik) { // добавляем массив
-														// студентов
+	public void setStudentsArray(Student[] studentik) { // add Array of student
+														//
 		if (studentik != null) {
 			this.studentik = studentik;
 		}
 
 	}
 
-	public Student[] getStudents() { // получить информацию о студенте
+	public Student[] getStudents() { // get Info about student
 		return this.studentik;
 	}
 
-	public void addStudetInGroup(Student student) { // добавляем студента в
-													// массив
+	public void addStudetInGroup(Student student) { // add student into Array
+													//
 		if (this.studentik != null) {
 			if (studentCounter < studentik.length) {
 				studentik[studentCounter] = student;
 				studentCounter++;
 			} else {
-				Student[] studentik = new Student[this.studentik.length + 5];
+				Student[] studentik = new Student[this.studentik.length + 1];
 				for (int i = 0; i < this.studentik.length; i++) {
 					studentik[i] = this.studentik[i];
 
@@ -35,21 +37,25 @@ public class Group {
 
 			}
 		} else {
-			this.studentik = new Student[5];
+			this.studentik = new Student[1];
 			this.studentik[studentCounter] = student;
 			studentCounter++;
 		}
-		this.studentik[0] = student;
+		// this.studentik[0] = student;
 
 	}
 
-	public int avgStudAge() { // считаем средний возраст студентов
+	public int avgStudAge() {
 		int avgAge;
 		int sum = 0;
-		for (int i = 0; i < studentik.length; i++) {
-			sum += studentik[i].getSudentAge(); // 1. создаем ссылку на обект. 2
-												// вызываем возр каждого
-												// студента
+		if (this.studentik == null) {
+		} else {// count average Age
+
+			for (int i = 0; i < studentik.length; i++) {
+				sum += studentik[i].getSudentAge();
+			} // 1. create link on object. 2
+				// call age every student
+
 		}
 		avgAge = sum / studentik.length;
 		return avgAge;
@@ -58,16 +64,18 @@ public class Group {
 	public int coutStudIn2015() {
 		int countTel = 0;
 		for (int i = 0; i < studentik.length; i++) {
-			if (studentik[i].getSudentAddindate() == 2015){
-				 // 1. создаем ссылку на обект. 2 вызываем дату зачисления
-					// каждого студента и сравниваем равно ли 2015
-			countTel++;
-		}
+			if (studentik[i].getSudentAddindate() == 2015) {
+				// 1.create link on object. 2 call date adding student in group,
+				// every student and compare date 2015
+				countTel++;
+			}
 		}
 		return countTel;
 	}
 
-	public int countYearMaxSudentAdd() {   // перебираем массив если год совпадает с первым, то увеличиваем счётчик
+	public int countYearMaxSudentAdd() { // going around Array if yera ==
+											// position 1 => counter ++
+											//
 		int coutSameYears = 0;
 		int year = studentik[0].getSudentAddindate();
 		for (int i = 0; i < studentik.length; i++) {
@@ -90,6 +98,22 @@ public class Group {
 		}
 		return nameYear;
 
+	}
+
+	public void sortBoobles(){
+		for (int i = this.studentik.length-1; i > 0 ; i--) {
+			for(int j = 0 ; j < i ; j++) {
+				if(this.studentik[j+1] !=null)
+				if(this.studentik[j].getSudentAge() > this.studentik[j+1].getSudentAge(){
+					Student promezytok = this.studentik[j];
+					this.studentik[j] = this.studentik[j+1];
+					this.studentik[j+1] = promezytok;
+				}
+				
+			}
+			
+		}
+		
 	}
 
 }
--- by.vashkevich.Arrys/src/by/vashkevich/StudGroup/MainApp.java
+++ by.vashkevich.Arrys/src/by/vashkevich/StudGroup/MainApp.java
@@ -9,14 +9,17 @@ Student stud0 = new Student("Hellp", 18, 2015);
 Student stud1 = new Student("Me", 20, 2016);
 Student stud2 = new Student("Please", 50, 2015);
 Student stud3 = new Student("It's", 20, 2015);
-Student stud4 = new Student("Hard", 666, 2016);
-//Student stud5 = new Student("Insaine", 2000, 2017);
+Student stud4 = new Student("Hard", 19, 2016);
+Student stud5 = new Student("Insaine", 21, 2017);
+Student stud6 = new Student("AZZZ", 21, 2015);
 
 System.out.println(stud0.getSudentName()+ "  "+stud0.getSudentAge());
 System.out.println(stud1.getSudentName()+ "  "+stud1.getSudentAge());
 System.out.println(stud2.getSudentName()+ "  "+stud2.getSudentAge());
 System.out.println(stud3.getSudentName()+ "  "+stud3.getSudentAge());
 System.out.println(stud4.getSudentName()+ "  "+stud4.getSudentAge());
+System.out.println(stud5.getSudentName()+ "  "+stud5.getSudentAge());
+System.out.println(stud6.getSudentName()+ "  "+stud6.getSudentAge());
 
 Group gr1 = new Group();
 
@@ -25,12 +28,14 @@ gr1.addStudetInGroup(stud1);
 gr1.addStudetInGroup(stud2);
 gr1.addStudetInGroup(stud3);
 gr1.addStudetInGroup(stud4);
-//gr1.addStudetInGroup(stud5);
+gr1.addStudetInGroup(stud5);
+gr1.addStudetInGroup(stud6);
 
 System.out.println("—редний возраст студентов : "+gr1.avgStudAge()+ "г." + " OK");
 System.out.println(" оличество студентов зачисленных в 2015г. : "+gr1.coutStudIn2015());
 System.out.println("√од в котором зачисленно максимальное число студентов : " +gr1.nameYearMaxSudentAdd());
 System.out.println(" оличество студентов в этот год : " + gr1.countYearMaxSudentAdd() + " OK");
+///System.out.println(gr1.sortBoobles());
 
 	}
 
